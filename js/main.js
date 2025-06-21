745b6bc45c3a03096049cadf9118febbcb9c04e5
Abdullah-Elgendy <abdullahelgendy38@gmail.com>
2025-06-21 08:52:03 +0300

    Added login and logout javascript logic + home page display + page redirection

--- js/main.js
+++ js/main.js
@@ -8,14 +8,45 @@ var registerName = document.querySelector("#registerName");
 var registerEmail = document.querySelector("#registerEmail");
 var registerPassword = document.querySelector("#registerPassword");
 
-//displayed username
+//home displayed username
 var userName = document.querySelector("#userName");
 
-//signup and login buttons
+//signup, login and logout buttons
 var signUpBtn = document.querySelector("#signUpBtn");
 var loginBtn = document.querySelector("#loginBtn");
+var logOutBtn = document.querySelector("#logOutBtn");
 
-//accounts JSON
+//logged in user
+var loginUser = null;
+
+//if loginUser exists
+if (JSON.parse(sessionStorage.getItem("loginUser"))) {
+  //get loginUser from sessionStorage
+  loginUser = JSON.parse(sessionStorage.getItem("loginUser"));
+
+  //if path is not index.html and not /
+  if (
+    window.location.pathname != "/index.html" &&
+    window.location.pathname != "/"
+  ) {
+    //open index.html and display the name
+    displayHome();
+  } else {
+    //if path is already correct then just display the name
+    userName.innerHTML = `Welcome ${loginUser.name}`;
+  }
+  //else if loginUser doesn't exist
+} else {
+  //if path is index.html or /
+  if (
+    window.location.pathname == "/index.html" ||
+    window.location.pathname == "/"
+  )
+    //redirect to login page
+    window.open("./login.html", "_self");
+}
+
+//accounts array
 var accounts = [];
 
 if (JSON.parse(localStorage.getItem("accounts"))) {
@@ -31,11 +62,15 @@ function validName() {
   var pattern = /^[a-zA-Z][a-zA-Z0-9_]{2,30}$/;
   var result = pattern.test(registerName.value);
   if (!result) {
-    document.querySelector(".name-error").classList.replace("d-none", "d-block");
+    document
+      .querySelector(".name-error")
+      .classList.replace("d-none", "d-block");
     registerName.classList.remove("is-valid");
     registerName.classList.add("is-invalid");
   } else {
-    document.querySelector(".name-error").classList.replace("d-block", "d-none");
+    document
+      .querySelector(".name-error")
+      .classList.replace("d-block", "d-none");
     registerName.classList.remove("is-invalid");
     registerName.classList.add("is-valid");
   }
@@ -47,13 +82,16 @@ function validEmail() {
   var pattern = /^[\w.%+-]+@[a-zA-Z0-9.-]{2,}.[a-zA-Z]{2,}$/;
   var result = pattern.test(registerEmail.value);
   if (!result) {
-    document.querySelector(".email-error").classList.replace("d-none", "d-block");
+    document
+      .querySelector(".email-error")
+      .classList.replace("d-none", "d-block");
     registerEmail.classList.remove("is-valid");
     registerEmail.classList.add("is-invalid");
     return false;
-  } 
-  else {
-    document.querySelector(".email-error").classList.replace("d-block", "d-none");
+  } else {
+    document
+      .querySelector(".email-error")
+      .classList.replace("d-block", "d-none");
     return emailUnique();
   }
 }
@@ -62,13 +100,17 @@ function validEmail() {
 function emailUnique() {
   for (var i = 0; i < accounts.length; i++) {
     if (registerEmail.value == accounts[i].email) {
-      document.querySelector(".email-exists").classList.replace("d-none", "d-block");
+      document
+        .querySelector(".email-exists")
+        .classList.replace("d-none", "d-block");
       registerEmail.classList.remove("is-valid");
       registerEmail.classList.add("is-invalid");
       return false;
     }
   }
-  document.querySelector(".email-exists").classList.replace("d-block" , "d-none");
+  document
+    .querySelector(".email-exists")
+    .classList.replace("d-block", "d-none");
   registerEmail.classList.remove("is-invalid");
   registerEmail.classList.add("is-valid");
   return true;
@@ -77,15 +119,19 @@ function emailUnique() {
 //password length between 8-30
 //must contain at least one character (?=.*[a-z]) and at least one digit (?=.*\d)
 //can't contain white spaces (?=\S+)
-function validPassword(){
+function validPassword() {
   var pattern = /^(?=.*[a-z])(?=.*\d)(?=\S+)[A-Za-z\d@.#$!%*?&]{8,30}$/;
   var result = pattern.test(registerPassword.value);
   if (!result) {
-    document.querySelector(".password-error").classList.replace("d-none", "d-block");
+    document
+      .querySelector(".password-error")
+      .classList.replace("d-none", "d-block");
     registerPassword.classList.remove("is-valid");
     registerPassword.classList.add("is-invalid");
   } else {
-    document.querySelector(".password-error").classList.replace("d-block", "d-none");
+    document
+      .querySelector(".password-error")
+      .classList.replace("d-block", "d-none");
     registerPassword.classList.remove("is-invalid");
     registerPassword.classList.add("is-valid");
   }
@@ -94,6 +140,7 @@ function validPassword(){
 
 //======== Functions ========
 
+//function to add account to array
 function addAccount() {
   var account = {
     name: registerName.value,
@@ -104,17 +151,74 @@ function addAccount() {
   saveToLocalStorage();
 }
 
+//function to check if user exists to log in
+function loginAccount() {
+  for (var i = 0; i < accounts.length; i++) {
+    if (
+      loginEmail.value.toLowerCase() == accounts[i].email.toLowerCase() &&
+      loginPassword.value == accounts[i].password
+    ) {
+      loginUser = accounts[i];
+      //save the account in session storage
+      sessionStorage.setItem("loginUser", JSON.stringify(loginUser));
+      return true;
+    }
+  }
+  return false;
+}
+
+//function to save updated array in local storage
 function saveToLocalStorage() {
   localStorage.setItem("accounts", JSON.stringify(accounts));
 }
 
+//function to open index.html and display the username in the homepage
+function displayHome() {
+  window.open("./index.html", "_self");
+  userName.innerHTML = `Welcome ${loginUser.name}`;
+}
+
+//function to log out user
+function logOutAccount() {
+  sessionStorage.removeItem("loginUser");
+  window.open("./login.html", "_self");
+}
+
 //======== Event Listeners ========
 
-//event listener to call validation functions when signing up
-signUpBtn.addEventListener("click", function () {
-  if (validName() && validEmail() && validPassword()) {
-    addAccount();
-  } else {
-    console.log("invalid");
-  }
-});
+//event listener on sign up button to call validation functions when signing up
+if (signUpBtn != null) {
+  signUpBtn.addEventListener("click", function () {
+    if (validName() && validEmail() && validPassword()) {
+      addAccount();
+      window.open("./login.html", "_self");
+    }
+  });
+}
+
+//event listener on log in button to check if user exists
+if (loginBtn != null) {
+  loginBtn.addEventListener("click", function () {
+    if (loginAccount()) {
+      document
+        .querySelector(".invalid-login")
+        .classList.replace("d-block", "d-none");
+        loginEmail.classList.remove("is-invalid");
+        loginPassword.classList.remove("is-invalid");
+      displayHome();
+    } else {
+      document
+        .querySelector(".invalid-login")
+        .classList.replace("d-none", "d-block");
+        loginEmail.classList.add("is-invalid");
+        loginPassword.classList.add("is-invalid");
+    }
+  });
+}
+
+//event listener on log out button
+if (logOutBtn != null) {
+  logOutBtn.addEventListener("click", function () {
+    logOutAccount();
+  });
+}
